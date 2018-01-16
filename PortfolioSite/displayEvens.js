d74dc59fc6b61e4db4cf89ee424f5742c30928b5
DaveO'Brien <david.o'brien@libertymutual.com>
2018-01-16 16:56:28 -0500

    1/16

--- PortfolioSite/displayEvens.js
+++ PortfolioSite/displayEvens.js
@@ -1,16 +1,28 @@
 
 
-function listEvens(start, stop, steps) { 
+function listEvens() { 
 
- alert(start)
-  if (isNaN(start)){
+    /*
+var start = document.getElementsByName('StartNumber')[0].value;
+var end = document.getElementsByName('EndNumber')[0].value;
+var steps = document.getElementsByName('step')[0].value;
+*/
+var steps = document.getElementsByName('step')[0].value;
+document.getElementById("evenResults").style.display = "";
+
+//var start = document.forms["formevens"]["StartNumber"].value;
+//alert(start);
+
+
+ /*   alert(start)
+    if (isNaN(start)) {
         alert(start)
     }
-   else {
-            document.getElementById("evenResults").style.display = "";
- 
-   }
-  
+    else {
+        document.getElementById("evenResults").style.display = "";
+
+    }
+*/
 
 }
 
